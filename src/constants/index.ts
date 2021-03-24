c5eeb91952c5bfb32e54344a57fbf0dac623fbe9
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-23 20:39:18 -0700

    feat: Store constants for visibility

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+/*
+ *  Mobile first responsive breakpoints based on Bootstrap.
+ *  https://getbootstrap.com/docs/4.0/layout/overview/#responsive-breakpoints
+ */
+export const BREAKPOINTS = {
+  sm: '(min-width: 576px)',
+  med: '(min-width: 768px)',
+  lg: '(min-width: 992px)',
+};
