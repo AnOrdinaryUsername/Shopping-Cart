52b75dfcb0e4b74e2b79c7374d5a0d03ff6d6637
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-25 22:27:47 -0700

    feat: Use ThemeProvider for bp constants

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,21 +1,25 @@
+import { Header } from 'components/organisms';
+import { About, Home, Stuff } from 'components/pages';
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import { Header } from '../components/organisms';
-import { About, Home, Stuff } from '../components/pages';
+import { ThemeProvider } from 'styled-components';
+import { THEME } from '../constants';
 import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <GlobalStyles />
-      <Header />
-      <main>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/stuff" component={Stuff} />
-        </Switch>
-      </main>
+    <HashRouter>
+      <ThemeProvider theme={THEME}>
+        <GlobalStyles />
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/stuff" component={Stuff} />
+          </Switch>
+        </main>
+      </ThemeProvider>
     </HashRouter>
   );
 }
