6170b58ad1a606514d6dfe37ed05b5343a833a7a
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-27 15:07:01 -0700

    feat: Add additional reusable constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,32 @@
+import { DefaultTheme } from 'styled-components/macro';
+
+export const BREAKPOINT_SIZES = {
+  sm: 576,
+  med: 768,
+  lg: 992,
+};
+
 /*
  *  Mobile first responsive breakpoints based on Bootstrap.
  *  https://getbootstrap.com/docs/4.0/layout/overview/#responsive-breakpoints
  */
 export const BREAKPOINTS = {
-  sm: '(min-width: 576px)',
-  med: '(min-width: 768px)',
-  lg: '(min-width: 992px)',
+  sm: `(min-width: ${BREAKPOINT_SIZES.sm}px)`,
+  med: `(min-width: ${BREAKPOINT_SIZES.med}px)`,
+  lg: `(min-width: ${BREAKPOINT_SIZES.lg}}px)`,
+};
+
+export const COLORS = {
+  gray: {
+    '100': 'hsl(208, 21%, 93%)',
+    '300': 'hsl(210, 16%, 76%)',
+    '400': 'hsl(208, 12%, 58%)',
+    '500': 'hsl(207, 12%, 43%',
+    '900': 'hsl(209, 15%, 28%)',
+  },
+  current: 'currentColor',
+};
+
+export const THEME: DefaultTheme = {
+  breakpoints: BREAKPOINTS,
 };
