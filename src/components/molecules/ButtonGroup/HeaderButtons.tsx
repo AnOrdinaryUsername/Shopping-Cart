98c660022df5615b1059a5c0e717282533138924
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-27 15:07:36 -0700

    feat: Add isMobile prop to change div size

--- src/components/molecules/ButtonGroup/HeaderButtons.tsx
+++ src/components/molecules/ButtonGroup/HeaderButtons.tsx
@@ -1,33 +1,29 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Anchor, Button } from 'components/atoms';
+import { ButtonGroup } from 'components/molecules';
 import React from 'react';
-import { Anchor, Button } from '../../atoms';
-import { ButtonGroup } from '../../molecules';
 
 interface HeaderButtonsProps {
+  isMobile?: boolean;
   onClick: () => void;
   theme: boolean;
 }
 
-const HeaderButtons = ({ onClick, theme }: HeaderButtonsProps) => {
+const HeaderButtons = ({ isMobile, onClick, theme }: HeaderButtonsProps) => {
   return (
-    <>
-      <ButtonGroup>
-        <Anchor
-          aria-label="View the source code repository"
-          link="https://github.com/AnOrdinaryUsername/Shopping-Cart"
-        >
-          <FontAwesomeIcon icon={faGithub} />
-        </Anchor>
-        <Button
-          aria-label={theme ? 'Activate dark theme' : 'Activate light theme'}
-          onClick={onClick}
-        >
-          <FontAwesomeIcon icon={theme ? faSun : faMoon} />
-        </Button>
-      </ButtonGroup>
-    </>
+    <ButtonGroup isMobile={isMobile}>
+      <Anchor
+        aria-label="View the source code repository"
+        link="https://github.com/AnOrdinaryUsername/Shopping-Cart"
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </Anchor>
+      <Button aria-label={theme ? 'Activate dark theme' : 'Activate light theme'} onClick={onClick}>
+        <FontAwesomeIcon icon={theme ? faSun : faMoon} />
+      </Button>
+    </ButtonGroup>
   );
 };
 
