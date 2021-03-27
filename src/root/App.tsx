1d85ee9e2109891f05818642b884a10f715f9d2e
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-27 15:02:18 -0700

    feat: Pass toggle state to mobile Header

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,27 +1,48 @@
 import { Header } from 'components/organisms';
-import { About, Home, Stuff } from 'components/pages';
+import { About, Home, NotFound, Stuff } from 'components/pages';
+import { useToggle, useWindowSize } from 'hooks';
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { THEME } from '../constants';
+import styled, { ThemeProvider } from 'styled-components/macro';
+import { BREAKPOINT_SIZES, THEME } from '../constants';
 import GlobalStyles from './GlobalStyles';
 
-function App() {
+const App = () => {
+  const [isToggled, setIsToggled] = useToggle();
+  const { width } = useWindowSize();
+  const isMobile = width < BREAKPOINT_SIZES.med;
+
   return (
     <HashRouter>
       <ThemeProvider theme={THEME}>
         <GlobalStyles />
-        <Header />
+        <Header isMobile={isMobile} isToggled={isToggled} onClick={setIsToggled} />
         <main>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/stuff" component={Stuff} />
+            <Route exact path="*" component={NotFound} />
           </Switch>
         </main>
+        {isMobile && <Overlay isMobileMenuPresent={isToggled} />}
       </ThemeProvider>
     </HashRouter>
   );
-}
+};
+
+const Overlay = styled.div`
+  ${(props: { isMobileMenuPresent: boolean }) =>
+    props.isMobileMenuPresent ? 'display: block;' : 'display: none;'}
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  /* top matches the height of header */
+  top: 8.4rem;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 
 export default App;
