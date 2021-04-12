af22fde99e43baa153e210cd52f4fcc784bdbf65
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-04-12 10:19:44 -0700

    feat: Create a FAQ page

---
+++ src/components/pages/FAQ.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FAQ = () => {
+  return (
+    <Wrapper>
+      <p>bottom text</p>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default FAQ;
