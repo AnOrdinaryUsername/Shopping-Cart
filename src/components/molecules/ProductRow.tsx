35e197667fead534d331a1f753752d00dc3fe432
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-04-12 19:43:09 -0700

    feat: Add some functionality to Cart page

--- src/components/molecules/ProductRow.tsx
+++ src/components/molecules/ProductRow.tsx
@@ -1,50 +1,123 @@
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Image } from 'components/atoms';
-import React, { MouseEventHandler, useContext } from 'react';
+import { useWindowSize } from 'hooks';
+import React, { ChangeEvent, MouseEventHandler, useContext, useState } from 'react';
 import LazyLoad from 'react-lazyload';
 import CartContext from 'root/CartContext';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
+import { BREAKPOINT_SIZES } from '../../constants';
 import { ProductItemProps } from './ProductItem';
 
 interface ProductRowProps extends ProductItemProps {
   removeItem: MouseEventHandler<HTMLButtonElement>;
+  quantityId: string;
 }
 
-const ProductRow = ({ alt, removeItem, price, productName, src }: ProductRowProps) => {
-  const { cartStorage } = useContext(CartContext);
+const ProductRow = ({ alt, removeItem, price, productName, src, quantityId }: ProductRowProps) => {
+  const { cartStorage, setCartStorage } = useContext(CartContext);
+  const { width } = useWindowSize();
+  const hasHamburgerMenu = width < BREAKPOINT_SIZES.med;
   const product = cartStorage.filter((item) => item.productName === productName);
+  const quantity = product[0].quantity;
+  const [itemQuantity, setItemQuantity] = useState(quantity);
+
+  const total = quantity * price;
+  const TWO_DECIMAL_PLACES = 100;
+  const roundedPrice =
+    Math.round((total + Number.EPSILON) * TWO_DECIMAL_PLACES) / TWO_DECIMAL_PLACES;
+
+  const updateQuantity = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    const updatedCart = cartStorage.map((item) =>
+      `${item.productName}${quantityId}` === name
+        ? {
+            ...item,
+            quantity: Number(value),
+          }
+        : item
+    );
+
+    setItemQuantity(Number(value));
+    setCartStorage(updatedCart);
+  };
 
   return (
     <Wrapper>
       <Data>
-        <LazyLoad height={'100%'} once>
-          <Image alt={alt} src={src} />
-        </LazyLoad>
-        <ProductInfo>
-          <Name>{productName}</Name>
-          <Quantity>QTY: {product[0].quantity}</Quantity>
-          <Price>${price}</Price>
-          <RemoveButton variant="tertiary" size="small" onClick={removeItem}>
-            <FontAwesomeIcon icon={faTrash} />
-            Remove Item
-          </RemoveButton>
-        </ProductInfo>
+        <FlexRow>
+          <LazyLoad height={'100%'} once>
+            <Image alt={alt} src={src} />
+          </LazyLoad>
+          <ProductInfo>
+            <Name>{productName}</Name>
+            <Price>${price}</Price>
+            <RemoveButton variant="tertiary" size="small" name={productName} onClick={removeItem}>
+              <FontAwesomeIcon icon={faTrash} />
+              Remove Item
+            </RemoveButton>
+          </ProductInfo>
+        </FlexRow>
       </Data>
+      {!hasHamburgerMenu && (
+        <td>
+          <SelectAmount
+            type="number"
+            value={itemQuantity}
+            min="1"
+            max="50"
+            pattern="[0-9]"
+            name={`${productName}${quantityId}`}
+            onInput={updateQuantity}
+          />
+        </td>
+      )}
+      <td>
+        <FinalPrice>${roundedPrice}</FinalPrice>
+        {hasHamburgerMenu && (
+          <SelectAmount
+            type="number"
+            value={itemQuantity}
+            min="1"
+            max="50"
+            pattern="[0-9]"
+            name={`${productName}${quantityId}`}
+            onInput={updateQuantity}
+          />
+        )}
+      </td>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.article`
-  display: flex;
+const Wrapper = styled.tr`
+  display: table-row;
+  line-height: 1.5;
+  box-shadow: 0px 1px 0px 0px hsl(0deg 0% 0% / 20%);
+
+  & > td {
+    display: table-cell;
+    vertical-align: middle;
+    text-align: right;
+  }
+
+  & > td:nth-child(2) {
+    text-align: center;
+  }
 `;
 
-const Data = styled.div`
-  display: flex;
-
+const Data = styled.td`
   & img {
-    max-width: 20rem;
+    max-width: 10rem;
+    border-radius: 0.5rem;
   }
+`;
+
+const FlexRow = styled.div`
+  display: flex;
+  padding: 2.4rem;
+  padding-left: 0;
+  text-align: left;
 `;
 
 const ProductInfo = styled.div`
@@ -52,30 +125,54 @@ const ProductInfo = styled.div`
   flex-direction: column;
   align-items: baseline;
   justify-content: center;
-  max-width: 20rem;
+  max-width: 40rem;
   width: 100%;
-  padding: 0 1.6rem;
+  padding: 0 3.2rem;
+
+  & > *:not(:last-child) {
+    padding-bottom: 0.6rem;
+  }
 `;
 
 const Name = styled.span`
-  font-weight: 600;
-`;
-
-const Quantity = styled.p`
-  font-size: 14px;
-  font-weight: 300;
+  font-weight: 800;
+  font-size: 1.25em;
+  line-height: 1;
 `;
 
 const Price = styled.span`
-  font-size: 1.25em;
+  font-size: 1em;
   font-weight: 700;
 `;
 
 const RemoveButton = styled(Button)`
+  font-size: 0.875em;
+  padding-left: 0;
+
   & > svg {
     align-items: center;
     margin-right: 0.8rem;
   }
+`;
+
+const SelectAmount = styled.input`
+  max-width: 7rem;
+  width: 100%;
+  font-size: 1em;
+  padding: 1.4rem;
+  text-align: center;
+  box-shadow: 0px 2px 4px 0px hsl(0deg 0% 0% / 20%);
+  outline: none;
+  appearance: none;
+  border: none;
+  border-radius: 0.5rem;
+`;
+
+const FinalPrice = styled.span`
+  font-size: 1.25em;
+  font-weight: 300;
+  padding: 1.6rem 3.2rem;
+  padding-right: 0;
 `;
 
 export default ProductRow;
--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,33 +1,53 @@
 import { ProductRow } from 'components/molecules';
+import { useWindowSize } from 'hooks';
 import React, { MouseEvent, useContext } from 'react';
 import CartContext from 'root/CartContext';
-import styled from 'styled-components';
-import uniqid from 'uniqid';
+import styled from 'styled-components/macro';
+import { BREAKPOINT_SIZES } from '../../constants';
+import { defaultButtonStyles, mediumButton, primaryButton } from '../atoms/Button';
 
 const Cart = () => {
   const { cartStorage, setCartStorage } = useContext(CartContext);
-  let cartContents = null;
+  const { width } = useWindowSize();
+  const hasHamburgerMenu = width < BREAKPOINT_SIZES.med;
+  const id = 'Quantity';
 
   const removeItem = (event: MouseEvent<HTMLButtonElement>) => {
-    const cell = event.currentTarget;
-    const productRow = cell.parentElement as HTMLTableRowElement;
-    const itemIndex = productRow.rowIndex;
-    const updatedCart = cartStorage.splice(itemIndex, 1);
-    setCartStorage(updatedCart);
+    event.preventDefault();
+    const { name } = event.currentTarget;
+    setCartStorage(cartStorage.filter((item) => item.productName !== name));
   };
-
-  if (cartStorage.length === 0) {
-    cartContents = <EmptyCart>Your cart is currently empty.</EmptyCart>;
-  } else {
-    cartContents = cartStorage.map((props) => (
-      <ProductRow removeItem={removeItem} key={uniqid()} {...props} />
-    ));
-  }
 
   return (
     <Wrapper>
-      <Title>Cart</Title>
-      {cartContents}
+      <Title>My Cart</Title>
+      {cartStorage.length === 0 ? (
+        <EmptyCart>Your cart is currently empty.</EmptyCart>
+      ) : (
+        <Form>
+          <Table>
+            <thead>
+              <TableRow>
+                <th scope="col">Product</th>
+                {!hasHamburgerMenu && <th scope="col">Quantity</th>}
+                <th scope="col">Total</th>
+              </TableRow>
+            </thead>
+            <tbody>
+              {cartStorage.map((props, index) => (
+                <ProductRow removeItem={removeItem} quantityId={id} key={index} {...props} />
+              ))}
+            </tbody>
+          </Table>
+          <OrderSummary>
+            <Subtotal>
+              <span>Subtotal</span>
+              <Amount>$ NaN</Amount>
+            </Subtotal>
+            <Checkout type="submit" name="checkout" value="Checkout" />
+          </OrderSummary>
+        </Form>
+      )}
     </Wrapper>
   );
 };
@@ -36,13 +56,77 @@ const EmptyCart = styled.p`
   text-align: center;
 `;
 
-const Wrapper = styled.div`
-  max-width: 80rem;
+const Wrapper = styled.section`
+  display: grid;
+  place-items: center;
+  padding: 1.4rem;
+
+  @media ${(p) => p.theme.breakpoints.sm} {
+    padding: 2.8rem;
+  }
+
+  @media ${(p) => p.theme.breakpoints.med} {
+    padding: 7.2rem;
+  }
+`;
+
+const Form = styled.form`
+  display: grid;
+  max-width: 60rem;
   width: 100%;
 `;
+
 const Title = styled.div`
   font-size: 2.25em;
   font-weight: 700;
+  padding-bottom: 2.8rem;
+`;
+
+const Table = styled.table`
+  text-align: right;
+`;
+
+const TableRow = styled.tr`
+  box-shadow: 0px 1px 0px 0px hsl(0deg 0% 0% / 20%);
+
+  & > th:first-child {
+    text-align: left;
+  }
+
+  & > th:nth-child(2) {
+    text-align: center;
+  }
+
+  & > th {
+    padding-top: 2.8rem;
+    padding-bottom: 2.8rem;
+  }
+`;
+
+const OrderSummary = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding-top: 3.2rem;
+`;
+
+const Subtotal = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.5em;
+  font-weight: 300;
+  width: fit-content;
+  padding-bottom: 1.6rem;
+`;
+
+const Amount = styled.span`
+  padding-left: 5.6rem;
+`;
+
+const Checkout = styled.input`
+  ${defaultButtonStyles}
+  ${mediumButton}
+  ${primaryButton}
 `;
 
 export default Cart;
